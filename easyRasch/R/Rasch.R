eb7724dacfb24f9167bf5b1c15077dc951888262
Rohan Gupta <rohangupta@wustl.edu>
2018-03-19 16:28:51 -0500

    Created and documented the package

--- easyRasch/R/Rasch.R
+++ easyRasch/R/Rasch.R
@@ -37,10 +37,10 @@ setMethod("initialize", "Rasch",
 
 #' @export
 setValidity("Rasch", function(object){
-  if(class(object@name)!="character"){return(@name is not a valid character)}
-  if(class(object@a)!="numeric"){return(@a is not a valid numeric)}
-  if(class(object@yj!="numeric")){return(@yj is not a valid numeric)}
-  if(length(object@name)!=1){return(@name is not a valid length)}
-  if(length(object@a)!=length(object@yj)){return(@a and @yj must have length n)}
+  if(class(object@name)!="character"){return("@name is not a valid character")}
+  if(class(object@a)!="numeric"){return("@a is not a valid numeric")}
+  if(class(object@yj!="numeric")){return("@yj is not a valid numeric")}
+  if(length(object@name)!=1){return("@name is not a valid length")}
+  if(length(object@a)!=length(object@yj)){return("@a and @yj must have length n")}
 }
 )
