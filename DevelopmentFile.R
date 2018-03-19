03f607afe59825434a868053a23c06017ec2dbb9
Rohan Gupta <rohangupta@wustl.edu>
2018-03-19 16:51:34 -0500

    Included some example code showing how each function works

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -7,3 +7,18 @@ setwd("/Users/rohangupta/Documents/WUSTL/SP2018/Pol Sci/Midterm")
 current.code<-as.package("easyRasch")
 load_all(current.code)
 document(current.code)
+
+#Example
+student<-new("Rasch")
+student@name<-"Rohan Gupta"
+student@a<-1:5
+student@yj<-c(1,0,1,0,1)
+student
+new("Rasch",name=1,a=1:5,yj=c(1,0,1,0,1))
+new("Rasch",name="Rohan Gupta",a="Rohan Gupta",yj=c(1,0,1,0,1))
+new("Rasch",name="Rohan Gupta",a=1:5,yj="Rohan Gupta")
+probability(student,5)
+likelihood(student,5)
+prior(5)
+eap(student)
+print.raschObj(student)
